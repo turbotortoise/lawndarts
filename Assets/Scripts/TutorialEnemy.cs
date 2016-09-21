a5afee93c7f8a8219a12e9f78d9d164c95769de8
turbotortoise <redfoot529@gmail.com>
2016-09-21 11:31:46 -0400

    added a tutorial scene, and stationary enemy in scene

---
+++ Assets/Scripts/TutorialEnemy.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialEnemy : MonoBehaviour {
+
+	Vector3 origPos;
+    MainPlayer player;
+
+
+	void Start () {
+        origPos = transform.position; //where the enemy spawns, not ready yet
+        tag = "Enemy";
+        player = GameObject.Find("Player").GetComponent<MainPlayer>();
+	}
+	
+	void FixedUpdate () {
+		//always rotates to face player
+		transform.rotation = Quaternion.LookRotation(
+            player.transform.position - transform.position);
+	}
+}
