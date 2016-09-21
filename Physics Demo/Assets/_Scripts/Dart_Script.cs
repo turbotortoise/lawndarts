4f4ab7e378017bc9c2d9c63ee2af7f6938626dcf
Ricardo Tucker <ricardo@Ricardos-iMac.wv.cc.cmu.edu>
2016-09-21 17:09:02 -0400

    added physics demo project

---
+++ Physics Demo/Assets/_Scripts/Dart_Script.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dart_Script : MonoBehaviour {
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (rb.velocity.y);
+	}
+
+	void OnCollisionEnter(Collision col) {
+		if (col.gameObject.tag == "Enemy") {
+			Destroy (col.gameObject);
+			Destroy (this.gameObject);
+		}
+	}
+}
---
+++ Physics Demo/Assets/_Scripts/Throw_Script.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Throw_Script : MonoBehaviour {
+	public GameObject dart_prefab;
+	public GameObject launch_point;
+
+	private Transform launch;
+
+	// Use this for initialization
+	void Start () {
+		launch = launch_point.transform;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire1")) {
+			GameObject dart = (GameObject) Instantiate (dart_prefab, launch.position, launch.rotation);
+			dart.GetComponent<Rigidbody> ().AddForce (launch.forward * 500f + (new Vector3(0, 500f, 0)));
+		}
+	}
+}
