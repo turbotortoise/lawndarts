a340666426b33d20599b649e2e5fa6badc9835d3
turbotortoise <redfoot529@gmail.com>
2016-09-29 11:57:11 -0400

    still trying to fix commit things

---
+++ Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemySpawn : MonoBehaviour {
+
+
+    public GameObject enemy;
+    public Transform[] spawnPoints;
+    public float spawnTime = 10f; // * 0.float depending on level
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("Spawn", spawnTime, spawnTime);
+	}
+
+	void Spawn() {
+		int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+		Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+	}
+}
